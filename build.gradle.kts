8d4cb84f108c971eb34615bed623a084ad048afc
DDatiOSCheat <datthuongGK00@gmail.com>
2026-04-13 16:01:56 +0700

    Fix!

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,6 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }  // Thêm repo cho libsu
-    }
-}
-
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(layout.buildDirectory)
 }
